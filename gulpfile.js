42af79d38893889db358169e6d9aed3127a3a5e0
Kim Røen <kim@kimroen.com>
2016-06-04 18:15:41 +0200

    Refactor gulpfile for duplication

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,54 @@ var autoprefixer = require('gulp-autoprefixer');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 
+var styleSource = 'src/sass/**/*.scss';
+var scriptSource = 'src/js/**/*.js';
+
+function processStyles(stream) {
+    return stream
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concat('screen.css'))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }));
+}
+
+function processScripts(stream) {
+  return stream
+    .pipe(babel({
+      presets: ['es2015']
+    }));
+}
+
 gulp.task('styles', function() {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('screen.css'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
+  var stream = gulp.src(styleSource)
+    .pipe(sourcemaps.init());
+
+  return processStyles(stream)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('production-styles', function() {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('screen.css'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
+  var stream = gulp.src(styleSource);
+
+  return processStyles(stream)
     .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+  var stream = gulp.src(scriptSource)
+    .pipe(sourcemaps.init());
+
+  return processScripts(stream)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets/js'));
 })
 
 gulp.task('production-scripts', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+  var stream = gulp.src(scriptSource);
+
+  return processScripts(stream)
     .pipe(gulp.dest('assets/js'));
 })
 
@@ -50,6 +61,6 @@ gulp.task('production', ['production-styles', 'production-scripts']);
 gulp.task('default', ['styles', 'scripts']);
 
 gulp.task('watch', ['default'], function() {
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch('src/sass/**/*.scss', ['styles']);
+  gulp.watch(scriptSource, ['scripts']);
+  gulp.watch(styleSource, ['styles']);
 });
