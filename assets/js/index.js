ca6440f6f2484f9218735a0f6ef844d446ad6eed
Kim Røen <kim@kimroen.com>
2016-06-04 17:03:20 +0200

    Change file indenting to two spaces

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,107 +6,107 @@
 
 /* globals jQuery, document */
 (function ($, undefined) {
-    "use strict";
+  "use strict";
 
-    var $document = $(document);
+  var $document = $(document);
 
-    $document.ready(function () {
+  $document.ready(function () {
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+    var $postContent = $(".post-content");
+    $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
+    $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
-
-        $('#google-form').activateGoogleForm();
-
-        $('#postnummer').activatePostalLookup();
+    $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
+      e.preventDefault();
+      $("body").toggleClass("nav-opened nav-closed");
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $('#google-form').activateGoogleForm();
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-            allOptions = $.extend(defaults, options);
+    $('#postnummer').activatePostalLookup();
+  });
 
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = $this.attr('data-offset') ? $this.attr('data-offset') : false,
-                position = $this.attr('data-position') ? $this.attr('data-position') : false,
-                toMove;
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
 
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({ scrollTop: $(this.hash).offset().top + toMove }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({ scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({ scrollTop: $(this.hash).offset().top }, allOptions.speed);
-            }
-        });
-    };
+    var defaults = {
+      elem: $(this),
+      speed: 500
+    },
+        allOptions = $.extend(defaults, options);
 
-    $.fn.activateGoogleForm = function () {
-        var url = this.attr('action');
-        var resultMessage = this.data('result-message');
+    allOptions.elem.click(function (event) {
+      event.preventDefault();
+      var $this = $(this),
+          $htmlBody = $('html, body'),
+          offset = $this.attr('data-offset') ? $this.attr('data-offset') : false,
+          position = $this.attr('data-position') ? $this.attr('data-position') : false,
+          toMove;
 
-        this.one('submit', function (e) {
-            var $form = $(this);
-            var values = $form.serialize();
-            var submitURL = url;
-            submitURL += '?';
-            submitURL += values;
-            submitURL += '&submit=Submit';
-            e.target.action = submitURL;
+      if (offset) {
+        toMove = parseInt(offset);
+        $htmlBody.stop(true, false).animate({ scrollTop: $(this.hash).offset().top + toMove }, allOptions.speed);
+      } else if (position) {
+        toMove = parseInt(position);
+        $htmlBody.stop(true, false).animate({ scrollTop: toMove }, allOptions.speed);
+      } else {
+        $htmlBody.stop(true, false).animate({ scrollTop: $(this.hash).offset().top }, allOptions.speed);
+      }
+    });
+  };
 
-            var $resultMessage = $('<div>', {
-                class: 'has-text-centered',
-                text: resultMessage
-            }).hide();
+  $.fn.activateGoogleForm = function () {
+    var url = this.attr('action');
+    var resultMessage = this.data('result-message');
 
-            $form.before($resultMessage).fadeOut(function () {
-                $resultMessage.fadeIn();
-            });
-        });
-    };
+    this.one('submit', function (e) {
+      var $form = $(this);
+      var values = $form.serialize();
+      var submitURL = url;
+      submitURL += '?';
+      submitURL += values;
+      submitURL += '&submit=Submit';
+      e.target.action = submitURL;
 
-    $.fn.activatePostalLookup = function () {
-        var $elem = $(this);
-        var $poststed = $($elem.data('place-element'));
-        var $loadingElement = $($elem.data('place-loading-element'));
+      var $resultMessage = $('<div>', {
+        class: 'has-text-centered',
+        text: resultMessage
+      }).hide();
 
-        $elem.on('change input', function (e) {
-            var postnummer = e.target.value;
+      $form.before($resultMessage).fadeOut(function () {
+        $resultMessage.fadeIn();
+      });
+    });
+  };
 
-            if (postnummer.length < 4) {
-                return $poststed.val('');
-            }
+  $.fn.activatePostalLookup = function () {
+    var $elem = $(this);
+    var $poststed = $($elem.data('place-element'));
+    var $loadingElement = $($elem.data('place-loading-element'));
 
-            $loadingElement.toggleClass('is-loading', true);
+    $elem.on('change input', function (e) {
+      var postnummer = e.target.value;
 
-            $.getJSON('https://api.bring.com/shippingguide/api/postalCode.json', {
-                clientUrl: window.location.href,
-                pnr: postnummer
-            }).done(function (response) {
-                var value = '';
-                if (response.valid) {
-                    value = response.result;
-                }
-                $poststed.val(value);
-            }).always(function () {
-                $loadingElement.toggleClass('is-loading', false);
-            });
-        });
-    };
+      if (postnummer.length < 4) {
+        return $poststed.val('');
+      }
+
+      $loadingElement.toggleClass('is-loading', true);
+
+      $.getJSON('https://api.bring.com/shippingguide/api/postalCode.json', {
+        clientUrl: window.location.href,
+        pnr: postnummer
+      }).done(function (response) {
+        var value = '';
+        if (response.valid) {
+          value = response.result;
+        }
+        $poststed.val(value);
+      }).always(function () {
+        $loadingElement.toggleClass('is-loading', false);
+      });
+    });
+  };
 })(jQuery);
--- src/js/index.js
+++ src/js/index.js
@@ -4,111 +4,111 @@
 
 /* globals jQuery, document */
 (function ($, undefined) {
-    "use strict";
+  "use strict";
 
-    var $document = $(document);
+  var $document = $(document);
 
-    $document.ready(function () {
+  $document.ready(function () {
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+    var $postContent = $(".post-content");
+    $postContent.fitVids();
 
-        $(".scroll-down").arctic_scroll();
+    $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
-
-        $('#google-form').activateGoogleForm();
-
-        $('#postnummer').activatePostalLookup();
+    $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+      e.preventDefault();
+      $("body").toggleClass("nav-opened nav-closed");
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $('#google-form').activateGoogleForm();
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+    $('#postnummer').activatePostalLookup();
+  });
 
-        allOptions = $.extend(defaults, options);
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
 
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
+    var defaults = {
+      elem: $(this),
+      speed: 500
+    },
 
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
+      allOptions = $.extend(defaults, options);
+
+    allOptions.elem.click(function (event) {
+      event.preventDefault();
+      var $this = $(this),
+          $htmlBody = $('html, body'),
+          offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+          position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+          toMove;
+
+      if (offset) {
+        toMove = parseInt(offset);
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+      } else if (position) {
+        toMove = parseInt(position);
+        $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+      } else {
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+      }
+    });
+
+  };
+
+  $.fn.activateGoogleForm = function() {
+    let url = this.attr('action');
+    let resultMessage = this.data('result-message');
+
+    this.one('submit', function(e) {
+      let $form = $(this);
+      let values = $form.serialize();
+      let submitURL = url;
+      submitURL += '?';
+      submitURL += values;
+      submitURL += '&submit=Submit';
+      e.target.action = submitURL;
+
+      let $resultMessage = $('<div>', {
+        class: 'has-text-centered',
+        text: resultMessage
+      }).hide();
+
+      $form
+        .before($resultMessage)
+        .fadeOut(() => { $resultMessage.fadeIn() });
+    });
+  };
+
+  $.fn.activatePostalLookup = function() {
+    let $elem = $(this);
+    let $poststed = $($elem.data('place-element'));
+    let $loadingElement = $($elem.data('place-loading-element'));
+
+    $elem.on('change input', function (e) {
+      let postnummer = e.target.value;
+
+      if (postnummer.length < 4) {
+        return $poststed.val('');
+      }
+
+      $loadingElement.toggleClass('is-loading', true);
+
+      $.getJSON('https://api.bring.com/shippingguide/api/postalCode.json', {
+        clientUrl: window.location.href,
+        pnr: postnummer
+      })
+        .done((response) => {
+          let value = '';
+          if (response.valid) {
+              value = response.result;
+          }
+          $poststed.val(value);
+        })
+        .always(() => {
+          $loadingElement.toggleClass('is-loading', false);
         });
-
-    };
-
-    $.fn.activateGoogleForm = function() {
-      let url = this.attr('action');
-      let resultMessage = this.data('result-message');
-
-      this.one('submit', function(e) {
-        let $form = $(this);
-        let values = $form.serialize();
-        let submitURL = url;
-        submitURL += '?';
-        submitURL += values;
-        submitURL += '&submit=Submit';
-        e.target.action = submitURL;
-
-        let $resultMessage = $('<div>', {
-          class: 'has-text-centered',
-          text: resultMessage
-        }).hide();
-
-        $form
-          .before($resultMessage)
-          .fadeOut(() => { $resultMessage.fadeIn() });
-      });
-    };
-
-    $.fn.activatePostalLookup = function() {
-        let $elem = $(this);
-        let $poststed = $($elem.data('place-element'));
-        let $loadingElement = $($elem.data('place-loading-element'));
-
-        $elem.on('change input', function (e) {
-            let postnummer = e.target.value;
-
-            if (postnummer.length < 4) {
-              return $poststed.val('');
-            }
-
-            $loadingElement.toggleClass('is-loading', true);
-
-            $.getJSON('https://api.bring.com/shippingguide/api/postalCode.json', {
-                clientUrl: window.location.href,
-                pnr: postnummer
-            })
-                .done((response) => {
-                    let value = '';
-                    if (response.valid) {
-                        value = response.result;
-                    }
-                    $poststed.val(value);
-                })
-                .always(() => {
-                    $loadingElement.toggleClass('is-loading', false);
-                });
-        });
-    };
+    });
+  };
 })(jQuery);
